c5315b5f31460bc82dcf409821b1c8d8666745eb
Xelit3 <javiru87@gmail.com>
2016-07-27 23:53:01 +0200

    20: Generate Business Domain Service for Tournaments
    Task-Url: https://github.com/Xelit3/GamersCreed/issues/20

---
+++ RocketStatsDomain/src/main/java/gamerscreed/rocketstats/domain/TournamentBusiness.java
@@ -0,0 +1,12 @@
+package gamerscreed.rocketstats.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import gamerscreed.rocketstats.model.dto.Player;
+import gamerscreed.rocketstats.model.dto.Tournament;
+
+public interface TournamentBusiness {
+
+	Tournament generateTournament(List<Player> aPlayerList, Date aBeginDate);
+}
---
+++ RocketStatsDomain/src/main/java/gamerscreed/rocketstats/domain/implementation/TournamentBusinessLayer.java
@@ -0,0 +1,21 @@
+package gamerscreed.rocketstats.domain.implementation;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import gamerscreed.rocketstats.domain.TournamentBusiness;
+import gamerscreed.rocketstats.model.dto.Player;
+import gamerscreed.rocketstats.model.dto.Tournament;
+
+@Service
+public class TournamentBusinessLayer implements TournamentBusiness{
+
+	@Override
+	public Tournament generateTournament(List<Player> aPlayerList, Date aBeginDate) {
+		
+		return null;
+	}
+
+}
