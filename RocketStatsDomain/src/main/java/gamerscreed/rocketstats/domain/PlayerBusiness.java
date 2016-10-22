551fbf7768b2362ef948965d3c5437d63c6e9cd6
Xelit3 <javiru87@gmail.com>
2016-10-23 00:31:31 +0200

    19: Generate Business Domain Service for Players
    Task-Url: https://github.com/Xelit3/GamersCreed/issues/19

--- RocketStatsDomain/src/main/java/gamerscreed/rocketstats/domain/PlayerBusiness.java
+++ RocketStatsDomain/src/main/java/gamerscreed/rocketstats/domain/PlayerBusiness.java
@@ -1,5 +1,17 @@
 package gamerscreed.rocketstats.domain;
 
+import java.util.List;
+
+import gamerscreed.rocketstats.model.dto.Player;
+
 public interface PlayerBusiness {
+	
+	public Player getPlayer();
+	
+	public Player updatePlayer();
+	
+	public boolean removePlayer();
+	
+	public List<Player> getAllPlayers();
 	
 }
--- RocketStatsDomain/src/main/java/gamerscreed/rocketstats/domain/implementation/PlayerBusinessLayer.java
+++ RocketStatsDomain/src/main/java/gamerscreed/rocketstats/domain/implementation/PlayerBusinessLayer.java
@@ -1,10 +1,37 @@
 package gamerscreed.rocketstats.domain.implementation;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import gamerscreed.rocketstats.domain.PlayerBusiness;
+import gamerscreed.rocketstats.model.dto.Player;
 
 @Service
 public class PlayerBusinessLayer implements PlayerBusiness {
+
+	@Override
+	public Player getPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player updatePlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removePlayer() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Player> getAllPlayers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
