4ca297bbf36aa540e06d1fad980525206123e834
Xelit3 <javiru87@gmail.com>
2016-11-06 20:42:07 +0100

    34: Fix Domain layer Sptring-boot startup on RocketStatsDomain
    Task-Url: https://github.com/Xelit3/GamersCreed/issues/34

--- RocketStatsCore/src/gamerscreed/rocketstats/model/dto/Player.java
+++ RocketStatsCore/src/gamerscreed/rocketstats/model/dto/Player.java
@@ -30,7 +30,7 @@ public class Player implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 
 	@Column(nullable=false)
