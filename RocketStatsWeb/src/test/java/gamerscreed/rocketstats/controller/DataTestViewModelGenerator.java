614fe5e2dbcc8cdeabaa83253519394be2afedc2
Xelit3 <javiru87@gmail.com>
2016-11-20 02:51:37 +0100

    39: Create Helper class to manage formatted classes
    Task-Url: https://github.com/Xelit3/GamersCreed/issues/39

---
+++ RocketStatsWeb/src/test/java/gamerscreed/rocketstats/controller/DataTestViewModelGenerator.java
@@ -0,0 +1,108 @@
+package gamerscreed.rocketstats.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import gamerscreed.rocketstats.beans.MatchPlayerStatisticViewBean;
+import gamerscreed.rocketstats.beans.MatchViewModel;
+import gamerscreed.rocketstats.beans.PlayerStatisticGridViewModel;
+import gamerscreed.rocketstats.beans.PlayerViewBean;
+import gamerscreed.rocketstats.beans.TournamentViewBean;
+import gamerscreed.rocketstats.components.UserSessionObject;
+
+public class DataTestViewModelGenerator {
+
+	public static UserSessionObject getTestUserSessionObject(){
+		UserSessionObject testPlayer = new UserSessionObject();
+		testPlayer.setUserName("XAVI");
+		testPlayer.setSessionId(java.util.UUID.randomUUID());
+		
+		return testPlayer;
+	}
+	
+	public static List<PlayerViewBean> getTestPlayerArray(){
+		List<PlayerViewBean> tmpPlayerList = new ArrayList<PlayerViewBean>();
+		
+		PlayerViewBean tmpTestBean = new PlayerViewBean();
+		tmpTestBean.setFullName("XAVI RUEDA");
+		tmpTestBean.setUserId(1);
+		tmpTestBean.setRoleName("JEFE");
+		tmpTestBean.setUserName("XELIT3");
+		tmpPlayerList.add(tmpTestBean);
+		
+		return tmpPlayerList;
+	}
+	
+	public static TournamentViewBean getTestTournamentView(){
+		TournamentViewBean testViewBean = new TournamentViewBean();
+		
+		testViewBean.dateStarted = new Date();
+		
+		PlayerStatisticGridViewModel testGridViewBean = new PlayerStatisticGridViewModel();
+		testGridViewBean.setPosition(1);
+		testGridViewBean.setUserName("Xelit3");
+		testGridViewBean.setGoalsFor(10);
+		testGridViewBean.setGoalsAgainst(2);
+		testGridViewBean.setMatchesLost(2);
+		testGridViewBean.setMatchesWon(5);
+		testGridViewBean.setPoints(22);
+		
+		testViewBean.getPlayerStatisticGridViewModelList().add(testGridViewBean);
+		
+		testGridViewBean = new PlayerStatisticGridViewModel();
+		testGridViewBean.setPosition(2);
+		testGridViewBean.setUserName("Noelia");
+		testGridViewBean.setGoalsFor(12);
+		testGridViewBean.setGoalsAgainst(15);
+		testGridViewBean.setMatchesLost(10);
+		testGridViewBean.setMatchesWon(0);
+		testGridViewBean.setPoints(0);
+		
+		testViewBean.getPlayerStatisticGridViewModelList().add(testGridViewBean);
+		
+		MatchViewModel tmpMatchViewModel = new MatchViewModel();
+		MatchPlayerStatisticViewBean tmpMatchPlayerStatisticViewBean = new MatchPlayerStatisticViewBean();
+		tmpMatchPlayerStatisticViewBean.setPlayerId(1);
+		tmpMatchPlayerStatisticViewBean.setUserName("Xelit3");
+		tmpMatchViewModel.setMatchId(1);
+		tmpMatchViewModel.getLocalPlayersStatistics().add(tmpMatchPlayerStatisticViewBean);
+		tmpMatchPlayerStatisticViewBean.setPlayerId(2);
+		tmpMatchPlayerStatisticViewBean.setUserName("Noelia");
+		tmpMatchViewModel.getVisitantPlayersStatistics().add(tmpMatchPlayerStatisticViewBean);
+		
+		testViewBean.getUndoneMatchViewModelList().add(tmpMatchViewModel);
+		
+		tmpMatchViewModel = new MatchViewModel();
+		tmpMatchViewModel.setMatchId(2);
+		tmpMatchPlayerStatisticViewBean.setPlayerId(1);
+		tmpMatchPlayerStatisticViewBean.setUserName("Xelit3");
+		tmpMatchViewModel.getLocalPlayersStatistics().add(tmpMatchPlayerStatisticViewBean);
+		tmpMatchPlayerStatisticViewBean.setPlayerId(2);
+		tmpMatchPlayerStatisticViewBean.setUserName("Noelia");
+		tmpMatchViewModel.getVisitantPlayersStatistics().add(tmpMatchPlayerStatisticViewBean);
+		tmpMatchViewModel.setGoalsLocal(5);
+		tmpMatchViewModel.setGoalsVisitor(2);
+		testViewBean.getDoneMatchViewModelList().add(tmpMatchViewModel);
+		
+		tmpMatchViewModel = new MatchViewModel();
+		tmpMatchViewModel.setMatchId(3);
+		
+		tmpMatchViewModel.getLocalPlayersStatistics().add(tmpMatchPlayerStatisticViewBean);
+		tmpMatchPlayerStatisticViewBean.setPlayerId(1);
+		tmpMatchPlayerStatisticViewBean.setUserName("Xelit3");
+		tmpMatchViewModel.getLocalPlayersStatistics().add(tmpMatchPlayerStatisticViewBean);
+		tmpMatchPlayerStatisticViewBean.setPlayerId(3);
+		tmpMatchPlayerStatisticViewBean.setUserName("Raul");
+		tmpMatchViewModel.getVisitantPlayersStatistics().add(tmpMatchPlayerStatisticViewBean);
+		tmpMatchPlayerStatisticViewBean.setPlayerId(1);
+		tmpMatchPlayerStatisticViewBean.setUserName("Angel");
+		tmpMatchViewModel.getVisitantPlayersStatistics().add(tmpMatchPlayerStatisticViewBean);
+		tmpMatchViewModel.setGoalsLocal(0);
+		tmpMatchViewModel.setGoalsVisitor(7);
+		testViewBean.getDoneMatchViewModelList().add(tmpMatchViewModel);
+		
+		return testViewBean;
+	}	
+	
+}
