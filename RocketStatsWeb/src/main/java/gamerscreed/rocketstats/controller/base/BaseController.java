9dd19871858cacb34c306c14915f29332c1ac1d2
Xelit3 <javiru87@gmail.com>
2017-02-04 17:23:58 +0100

    30: Generate view for match update
    Task-Url: https://github.com/Xelit3/GamersCreed/issues/30

--- RocketStatsWeb/src/main/java/gamerscreed/rocketstats/controller/base/BaseController.java
+++ RocketStatsWeb/src/main/java/gamerscreed/rocketstats/controller/base/BaseController.java
@@ -2,9 +2,6 @@ package gamerscreed.rocketstats.controller.base;
 
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import gamerscreed.rocketstats.utilities.WebServiceControllerCallDispatcher;
 
 public abstract class BaseController {
@@ -17,18 +14,5 @@ public abstract class BaseController {
 		callDispatcher = new WebServiceControllerCallDispatcher();
 	}
 	
-	public String serializeObjectToJson(Object aObject){
-		ObjectMapper mapper = new ObjectMapper();
-		String jsonStringObject = new String();
-		try {
-			jsonStringObject = mapper.writeValueAsString(aObject);
-		} 
-		catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}		
-		return jsonStringObject;		
-	}	
-	
-	public abstract ModelAndView viewIndex();
-		
+	public abstract ModelAndView viewIndex();	
 }
